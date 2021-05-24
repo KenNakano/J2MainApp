c0546e9daffd8f4ee7c6d15c32ab3a0e08763c60
akeda <ztm4D0as@gmail.com>
2021-05-24 14:54:27 +0900

    UI画面 ・お店アプリにログイン画面を設置した（shop_login.xml）、 ・ログアウトボタンをshop_top.xmlに追加した

--- app/src/main/java/com/sample/j2mainapp/OrderReceive.java
+++ app/src/main/java/com/sample/j2mainapp/OrderReceive.java
@@ -15,6 +15,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class OrderReceive extends AppCompatActivity {
 
@@ -74,6 +75,8 @@ public class OrderReceive extends AppCompatActivity {
 
                  }
              }
+
+
         );
 
 //        textView1 = (TextView) findViewById(R.id.content1);
